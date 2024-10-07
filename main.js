e766ad12f3f7fc8dc9877bb6304dbe7915e7788b
Diego Nehuen <nehuenvd@gmail.com>
2024-10-07 21:22:20 +0200

    comienzo de js movilidad sobre el paso del cursor

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let emptyElement = document.querySelector('.about .empty');
+let titleElement = document.querySelector('.about .title');
+
+window.addEventListener( "mousemove"  , handleMouseMove)
+
+function handleMouseMove( event ){
+  emptyElement.style.flexBasis =  event.clientX + "px";
+  titleElement.style.flexBasis =  event.clientY /2 + "px";
+}
+
+
