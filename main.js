671465dcd62f37eaf9e025d9f496e73d37882e37
Diego Nehuen <nehuenvd@gmail.com>
2024-10-09 17:22:18 +0200

    actualizacion y interaccion con mouse

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 
 let emptyElement = document.querySelector('.about .empty');
 let titleElement = document.querySelector('.about .title');
+let figureElements = document.querySelectorAll('.service figure')
 
 window.addEventListener( "mousemove"  , handleMouseMove)
 
 function handleMouseMove( event ){
+
   emptyElement.style.flexBasis =  event.clientX + "px";
-  titleElement.style.flexBasis =  event.clientY /2 + "px";
+  titleElement.style.flexBasis =  event.clientY / 2 + "px";
+
+  figureElements.forEach( function( element ) {
+    element.style.flexBasis = event.clientX + "px";
+  });
 }
 
 
